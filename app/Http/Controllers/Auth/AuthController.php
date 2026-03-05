59f96d589ad6de51f8549b16cc8e5253231f72bf
ze-fernando <zefernando.dev@gmail.com>
2026-03-05 11:56:15 -0300

    feat: logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             Auth::user();
-            return redirect()->route("feed");
+            return redirect()->route("posts.index");
         }
 
         return redirect()
@@ -44,12 +44,18 @@ class AuthController extends Controller
         try {
             $user = User::create($credentials);
             Auth::login($user);
-            return redirect()->route("feed");
+            return redirect()->route("posts.index");
         } catch (\Exception $e) {
             Log::error($e);
             return redirect()
                 ->back()
                 ->withErrors(["name" => "Registration failed"]);
         }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect()->route("show_auth");
     }
 }
